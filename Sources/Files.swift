7da9c9a4253b66daa826ebc461a1fabfd1db36b6
Robert Lefkowitz <r0ml@mac.com>
2025-07-17 23:20:52 -0400

    added a Swift version of basename from Libc

--- Sources/Files.swift
+++ Sources/Files.swift
@@ -1,5 +1,32 @@
-// Copyright (c) 1868 Charles Babbage
 // Modernized by Robert "r0ml" Lefkowitz <code@liberally.net> in 2025
+// from a files containing the following notices:
+
+/*
+ * Copyright (c) 1997 Todd C. Miller <Todd.Miller@courtesan.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
 // FIXME: do the swift-system version of this
@@ -392,6 +419,50 @@ public extension FileDescriptor {
 }
 
 
+public struct POSIXErrno: Error {
+    let code: Int32
 
+  public init(_ code: Int32 = errno) {
+    self.code = code
+  }
 
+  public var description : String {
+    return String(cString: strerror(code))
+  }
 
+  public var localizedDescription : String {
+    return description
+  }
+}
+
+public func basename(_ path : String) throws(POSIXErrno) -> String {
+  // Empty string gets treated as "."
+  if path.isEmpty {
+    return "."
+  }
+
+  var ppath = Substring(path)
+
+  // Strip any trailing slashes
+  while ppath.last == "/" {
+    ppath = ppath.dropLast()
+  }
+
+  // All slashes becomes "/"
+  if ppath.isEmpty {
+    return "/"
+  }
+
+  // Find the start of the base
+  var res = Substring("")
+  while !ppath.isEmpty && ppath.last != "/" {
+    res.insert(ppath.last!, at: ppath.startIndex)
+    ppath = ppath.dropLast()
+  }
+
+  if res.count >= MAXPATHLEN {
+    throw POSIXErrno(ENAMETOOLONG)
+  }
+
+  return String(res)
+}
