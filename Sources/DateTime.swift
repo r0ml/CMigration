4a327f26afefce9b26d4ef060feeb4c18b31902a
Robert Lefkowitz <r0ml@mac.com>
2026-01-31 15:03:44 -0500

    added ISO8601 date parsing

--- Sources/DateTime.swift
+++ Sources/DateTime.swift
@@ -29,5 +29,46 @@ public struct DateTime {
   public var timespec : Darwin.timespec {
     return Darwin.timespec(tv_sec: secs, tv_nsec: nanosecs)
   }
+
+  public init(fromISO8601 s: String) throws {
+    var tm = tm()
+    memset(&tm, 0, MemoryLayout<tm>.size)
+
+    let cstr = s.cString(using: .utf8)!
+
+    // Try full date-time with optional 'Z'
+    let formats = [
+      "%Y-%m-%dT%H:%M:%SZ",
+      "%Y-%m-%dT%H:%M:%S",
+      "%Y-%m-%d"
+    ]
+
+    var parsed = false
+    for fmt in formats {
+      if strptime(cstr, fmt, &tm) != nil {
+        parsed = true
+        break
+      }
+    }
+
+    guard parsed else {
+      throw POSIXErrno(EINVAL, fn: "parsing ISO8601 date string")
+    }
+
+    // Force UTC
+    tm.tm_isdst = 0
+
+    let seconds = timegm(&tm)
+    guard seconds >= 0 else {
+      throw POSIXErrno(EINVAL, fn: "parsing ISO8601 date string")
+    }
+
+    self.init(seconds)
+  }
+
+
+
 }
+
+
 
