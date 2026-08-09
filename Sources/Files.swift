e19866905957d6d9e113fd8cec0432f405fcf574
Robert Lefkowitz <r0ml@mac.com>
2026-08-09 11:18:27 -0400

    opening for writing should create the file

--- Sources/Files.swift
+++ Sources/Files.swift
@@ -85,7 +85,7 @@ extension FileDescriptor {
   /// - Parameter forWriting: The file path to open.
   /// - Throws: `Errno` if the file cannot be opened.
   public init(forWriting: String) throws {
-    self = try Self.open(forWriting, .writeOnly)
+    self = try Self.open(forWriting, .writeOnly, options: [.create, .truncate])
   }
 
   /// Opens `forUpdating` for both reading and writing.
