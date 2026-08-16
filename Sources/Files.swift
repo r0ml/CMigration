c7a10e2e1fb859073aa55f1de3f7fe9a2094f23c
Robert Lefkowitz <r0ml@mac.com>
2026-08-16 16:29:45 -0400

    need LATIN1 encoding for reading

--- Sources/Files.swift
+++ Sources/Files.swift
@@ -925,9 +925,17 @@ public extension FileDescriptor {
   ///
   /// - Returns: The full contents as a `String`.
   /// - Throws: Any error from ``readAllBytes()``.
-  func readAsString() throws -> String {
+  func readAsString<C : Unicode.Encoding>(encoding: C.Type = UTF8.self) throws -> String {
     let k = try readAllBytes()
-    return String(decoding: k, as: UTF8.self)
+    switch encoding {
+      case is ISOLatin1.Type:
+        // FIXME: should it be validating?
+        return String(decoding: k, as: ISOLatin1.self)
+      case is UTF8.Type:
+        fallthrough
+      default:
+        return String.init(decoding: k, as: UTF8.self)
+    }
   }
 
   /// Reads all bytes from this already-open descriptor (streaming; mmap does not apply).
